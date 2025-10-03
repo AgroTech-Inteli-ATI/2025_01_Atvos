8b4d79c95b7ba7f2c5fce15a42d51998773f8a66
MZMS1510 <msmarcondes10@gmail.com>
2025-10-03 18:13:12 -0300

    ✨feat: update model to better suit the actual data that will come from the API

--- server/km_track_api/models.py
+++ server/km_track_api/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
 class Vehicle(models.Model):
-    identifier = models.CharField(max_length=100, default='---')  # placa, frota, etc.
+    plate = models.CharField(max_length=6, unique=True)
+    identifier = models.IntegerField(unique=True)
+    bus_type = models.CharField(max_length=50, null=True, blank=True, help_text="Type of bus (Ativo/Reserva)")
 
     def __str__(self):
-        return self.identifier
+        return str(self.identifier)
 
 
 class TelemetryRecord(models.Model):
@@ -12,12 +14,43 @@ class TelemetryRecord(models.Model):
     driver = models.CharField(max_length=100, blank=True, null=True)
     date = models.DateTimeField()
 
-    odometer = models.FloatField()
-    distance = models.FloatField()
+    # departure_place = models.ForeignKey(Place, on_delete=models.SET_NULL, null=True, related_name="departures")
+    departure_km = models.FloatField()
+    departure_time = models.DateTimeField()
+    # arrival_place = models.ForeignKey(Place, on_delete=models.SET_NULL, null=True, related_name="arrivals")
+    arrival_km = models.FloatField()
+    arrival_time = models.DateTimeField()
+    total_time = models.DurationField()
+    total_km_traveled = models.FloatField()
+    max_speed_reached = models.FloatField()
+    people_transported = models.IntegerField()
+    occupancy_rate = models.FloatField(null=True, blank=True, help_text="Occupancy rate (percentage)")
     duration = models.DurationField()
     idle_time = models.DurationField()
 
+    SHIFT_CHOICES = [
+        ('morning', 'Morning'),
+        ('afternoon', 'Afternoon'),
+        ('night', 'Night'),
+    ]
+    shift = models.CharField(max_length=10, choices=SHIFT_CHOICES)
+
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"{self.vehicle} - {self.date.date()} - {self.distance} km"
+        return f"{self.vehicle} - {self.date.date()} - {self.total_km_traveled} km"
+
+class Operation(models.Model):
+    telemetry_record = models.ForeignKey(TelemetryRecord, on_delete=models.CASCADE, related_name="operations")
+    unity = models.CharField(max_length=3) # UAT, UCR, UMV, UAE, URC, UAL, UCP, UEL, USL
+    area = models.TextField(max_length=2)
+    operation_type = models.TextField(max_length=20)
+    shifts_accounted = models.IntegerField()
+    fix_cost = models.FloatField()
+    variable_cost = models.FloatField()
+    average_monthly_km = models.FloatField()
+    monthly_cost = models.FloatField()
+    cost_per_safra = models.FloatField(help_text="monthly_cost * 12")
+    average_occupancy = models.FloatField(help_text="Average occupancy rate (percentage)")
+    observations = models.TextField(blank=True, null=True)
