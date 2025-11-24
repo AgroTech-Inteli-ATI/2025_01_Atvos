af5e07349c403cffe559b69c6be61c1b897007f7
Marcos <msmarcondes10@gmail.com>
2025-11-24 19:41:36 -0300

    fix: add dotenv lib to requirements.txt

--- server/server/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-
-urlpatterns = [
-
-    path('api/', include('travels.urls')),
-    path('api/dashboard/', include('dashboard.urls')),
-    path("users/", include("users.urls")),
-]
